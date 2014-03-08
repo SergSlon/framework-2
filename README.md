972f72e7b32dc6a954ecad4c8375551011ec3f8f
Paweł Banach <pawlobanano@gmail.com>
2014-03-08 19:31:42 +0100

    Part 7. Making layers in our framework; Braking in to Controller and Model; Further code refactoring...; On a way to the better reusability.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 framework
 =========
 
-Create your own framework... on top of the Symfony2 Components. A series of articles that explains how to create a framework with the Symfony2 Components - by Fabien Potencier.
+[Create your own framework... on top of the Symfony2 Components. A series of articles that explains how to create a framework with the Symfony2 Components.](http://fabien.potencier.org/article/50/create-your-own-framework-on-top-of-the-symfony2-components-part-1)
+> by Fabien Potencier
--- src/app.php
+++ src/app.php
@@ -1,34 +1,10 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-
-class LeapYearController
-{
-    public function indexAction($year)
-    {
-        if (is_leap_year($year)) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-}
-
-function is_leap_year($year = null)
-{
-    if (null === $year) {
-        $year = date('Y');
-    }
-
-    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
-}
 
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is-leap-year/{year}', [
     'year'        => null,
-//    '_controller' => [new LeapYearController(), 'indexAction'],
-    '_controller' => 'LeapYearController::indexAction',
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 ]));
 
 return $routes;
